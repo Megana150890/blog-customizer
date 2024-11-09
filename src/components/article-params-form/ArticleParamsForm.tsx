2e4b44c046cadaaa1e6739d1271477d8f20453db
Megana <megana15081990@yandex.ru>
2024-11-09 21:25:28 +0700

    сделала выдвижную стрелку

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,19 +3,29 @@ import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 import clsx from 'clsx';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsopen] = useState(false);
+	const toogleForm = () => {
+		setIsopen(!isOpen);
+	};
 	return (
 		<>
-			<ArrowButton isOpen={true} onClick={() => {}} />
-			<aside className={clsx(styles.container, styles.container_open)}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
+			<ArrowButton isOpen={isOpen} onClick={toogleForm} />
+			{isOpen && (
+				<aside
+					className={clsx(styles.container, {
+						[styles.container_open]: isOpen,
+					})}>
+					<form className={styles.form}>
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' htmlType='reset' type='clear' />
+							<Button title='Применить' htmlType='submit' type='apply' />
+						</div>
+					</form>
+				</aside>
+			)}
 		</>
 	);
 };
